32dd082f59c33ffcf933b5a47561ea6adc84770e
Abdelhamid Bakhta <abdelhamid.bakhta@gmail.com>
2022-03-31 11:06:28 +0200

    ex02 claim_points

--- scripts/ex02.sh
+++ scripts/ex02.sh
@@ -8,3 +8,11 @@ cmd="$cmd --function my_secret_value"
 
 output=$(eval $cmd | tail -1)
 echo "Secret value: $output"
+
+echo "Invoking claim_points with secret value"
+starknet invoke \
+--address $EX02_ADDR \
+--abi $ABI_DIR/ex02.json \
+--network $NETWORK --account $ACCOUNT_ALIAS \
+--function claim_points \
+--inputs $output
